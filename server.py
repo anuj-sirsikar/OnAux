9cbf76ba8daf93556f311dc08bd4bc89c2d84277
Justin <m253642@usna.edu>
2024-10-01 09:43:49 -0400

    Request is not working

--- server.py
+++ server.py
@@ -1,57 +1,29 @@
 from fastapi import FastAPI
 import uvicorn
 import socket
-import time
-
-# app1 = FastAPI()
-
-
-# # Define routes for both servers
-# @app1.get("/")
-# def read_root_8080():
-#     return {"message": "Server 1 running on port 8080"}
-
-# # Function to get the IP address of the local machine
-# def get_ip_address():
-#     hostname = socket.gethostname()
-#     ip_address = socket.gethostbyname(hostname)
-#     return ip_address
-
-# # Run both servers
-# def run_servers():
-#     # Get the IP address of the local machine
-#     ip_address = get_ip_address()
-
-#     # Print IP address and ports
-#     print(f"Server 1 running at http://{ip_address}:8080")
-
-#     # Launch both servers concurrently using Uvicorn
-#     config1 = uvicorn.Config(app1, host="0.0.0.0", port=8080, log_level="info")
-    
-#     # Create separate servers for each app
-#     server1 = uvicorn.Server(config1)
-#     return server1
-# if __name__ == "__main__":
-#     server = run_servers()
-#     while(True):
-#         print()
-#         time.sleep(5)
-
-from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class SongCreate(BaseModel):
+    song_name: str
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+#Holder for 
+songs = []
+
+@app.post("/request/")
+async def store_song(song: SongCreate):
+    return song
+    # return {"message": f"Song '{song}' stored successfully", "total_songs": len(songs)}
+
 if __name__ == "__main__":
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
     uvicorn.run(app, host=ip_address, port=8080)
-
-
-
-
 
 
--- test_server.py
+++ test_server.py
@@ -3,6 +3,8 @@ import requests
 import time
 import socket
 
+
+
 def test_root_endpoint():
     # Start a server process we can test
     process = subprocess.Popen(["python", "server.py", "8080"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -14,6 +16,26 @@ def test_root_endpoint():
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
         response = requests.get("http://" + ip_address + ":8080/")
+        
+        # The assert command throws an error if a False value is passed to it.
+        # This is the command you will use to get your pytest work done.
+        assert response.status_code == 200
+    finally:
+        # Terminate the server process after the test
+        process.terminate()
+
+def test_request_endpoint():
+    # Start a server process we can test
+    process = subprocess.Popen(["python", "server.py", "8080"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # The server might take a second to start
+    time.sleep(1)
+    
+    try:
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        myobj = {'song': 'taco'}
+        response = requests.post("http://" + ip_address + ":8080/request/", json=myobj)
         
         # The assert command throws an error if a False value is passed to it.
         # This is the command you will use to get your pytest work done.
