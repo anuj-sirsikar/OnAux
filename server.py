2aac5b9c3def134c31a5fddf43d735dcbf0d7cc1
dababy9 <m256642@usna.edu>
2024-10-15 09:47:33 -0400

    .

--- server.py
+++ server.py
@@ -1,25 +1,33 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 import socket
 from pydantic import BaseModel
 
 app = FastAPI()
 
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
 class SongCreate(BaseModel):
     song_name: str
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+#@app.get("/")
+#async def root():
+    
 
 #Holder for 
 songs = []
 
-@app.post("/request/")
+@app.post("/request")
 async def store_song(song_name: SongCreate):
     return {"song": "Anything"}
     # return {"message": f"Song '{song}' stored successfully", "total_songs": len(songs)}
+
+@app.post("/search")
+async def search_song(song_name: SongCreate):
+    print("TRIED")
+    return {"song": "SEARCH"}
 
 if __name__ == "__main__":
     hostname = socket.gethostname()
