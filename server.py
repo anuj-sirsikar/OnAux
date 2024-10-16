1a8c21842ed79df83571e7c16365aaf13523d4b6
dababy9 <m256642@usna.edu>
2024-10-16 10:48:50 -0400

    server/client communication

--- server.py
+++ server.py
@@ -1,34 +1,35 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 import uvicorn
 import socket
 from pydantic import BaseModel
 
+# create app
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+# define pydantic object for endpoint POST body inputs
+class Text(BaseModel):
+    text: str
 
-class SongCreate(BaseModel):
-    song_name: str
+# define root endpoint (serve html page)
+@app.get("/")
+async def home():
+    f = open("index.html")
+    return HTMLResponse(content=f.read(), status_code=200)
 
-
-#@app.get("/")
-#async def root():
-    
-
-#Holder for 
-songs = []
-
+# define request endpoint (user requesting to add song to queue)
 @app.post("/request")
-async def store_song(song_name: SongCreate):
-    return {"song": "Anything"}
-    # return {"message": f"Song '{song}' stored successfully", "total_songs": len(songs)}
+async def store_song(body: Text):
+    song = body.dict()['text']
+    return {"message": "/request WORKED!"}
 
+# define search endpoint (user searching for song)
 @app.post("/search")
-async def search_song(song_name: SongCreate):
-    print("TRIED")
-    return {"song": "SEARCH"}
+async def search_song(body: Text):
+    text = body.dict()['text']
+    return {"message": "/search WORKED!"}
 
+# define main method to run server
 if __name__ == "__main__":
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
