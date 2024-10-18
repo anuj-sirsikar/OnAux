4c6f7fe92d6109ddbc82edd19e349c69091bbb0f
dababy9 <m256642@usna.edu>
2024-10-18 08:40:30 -0400

    reset?

--- test_server.py
+++ test_server.py
@@ -4,8 +4,9 @@ import time
 import socket
 
 
-
+# test root to ensure a good HTTP response and correct HTML page
 def test_root_endpoint():
+
     # Start a server process we can test
     process = subprocess.Popen(["python", "server.py", "8080"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
@@ -18,13 +19,16 @@ def test_root_endpoint():
         response = requests.get("http://" + ip_address + ":8080/")
         
         # The assert command throws an error if a False value is passed to it.
-        # This is the command you will use to get your pytest work done.
         assert response.status_code == 200
+        print(response.content)
+
+    # Terminate the server process after the test
     finally:
-        # Terminate the server process after the test
         process.terminate()
 
+# test request endpoint to ensure a POST request is handled and a response is generated
 def test_request_endpoint():
+
     # Start a server process we can test
     process = subprocess.Popen(["python", "server.py", "8080"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
@@ -34,12 +38,13 @@ def test_request_endpoint():
     try:
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
-        myobj = {'song_name' : 'test'}
+        myobj = {'text' : 'test'}
         response = requests.post("http://" + ip_address + ":8080/request/", json=myobj)
+
         # The assert command throws an error if a False value is passed to it.
-        # This is the command you will use to get your pytest work done.
         print(response.text)
         assert response.status_code == 200
+
+    # Terminate the server process after the test
     finally:
-        # Terminate the server process after the test
         process.terminate()
